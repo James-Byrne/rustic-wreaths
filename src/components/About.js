a8e8dac0235352b7e49487a701e0e232b551871b
James Byrne <jamesdesbyrne@gmail.com>
2017-07-05 18:51:22 +0100

    Adding a rough implementation of the About section and adding the correct font for titles

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className="about text-center container-fluid">
-        Hi
+      <div className="about container-fluid">
+        <div className="row">
+          <div className="col col-md-8 mx-auto text-center">
+            <h6>About Us</h6>
+            <p>
+              We design and handcraft high quality Noble Fir wreaths for wholesale. Rustic Wreaths is a sister company to Forest Fresh Christmas Tree farms and this partnership provides us with a continual supply of foliage. We are environmentally responsible and fully support sustainability by replanting trees every year.
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
