a1bc9d291b0392e3ef4fc29b26033a0c49465dfc
James Byrne <jamesdesbyrne@gmail.com>
2017-07-07 08:15:06 +0100

    Adding the contact form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  render() {
+    <div className="contact-form container-fluid">
+      Contact Form
+    </div>
+  }
+}
+
+export default ContactForm;
