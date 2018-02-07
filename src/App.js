38147aece42e2698f876773be47cb1324c7623e8
James Byrne <jamesdesbyrne@gmail.com>
2018-02-07 09:44:01 +0000

    Using a hash router and removing the service worker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import 'font-awesome/css/font-awesome.css'
 import './App.css';
